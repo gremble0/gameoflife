66ef794805f2d53437ee9598db24e03418018d60
gremble0 <hermanstornes@gmail.com>
2023-07-18 05:21:01 +0200

    Almost functional game

--- main.go
+++ main.go
@@ -40,11 +40,32 @@ func drawBoard(cells [ROWS][COLUMNS]string) {
 }
 
 func updateBoard(cells [ROWS][COLUMNS]string) [ROWS][COLUMNS]string {
-    for i := 0; i < ROWS; i++ {
-	for j := 0; j < COLUMNS; j++ {
+    for row := 0; row < ROWS; row++ {
+	for col := 0; col < COLUMNS; col++ {
+	    aliveNeighbors := 0
+	    // Row above
+	    if row > 0 {
+		if col > 0 && cells[row - 1][col - 1] == ALIVE { aliveNeighbors++ }
+		if cells[row - 1][col] == ALIVE { aliveNeighbors++ }
+		if col + 1 < COLUMNS && cells[row - 1][col + 1] == ALIVE { aliveNeighbors++ }
+	    }
 
+	    // Current row
+	    if col > 0 && cells[row][col - 1] == ALIVE { aliveNeighbors++ }
+	    if col + 1 < COLUMNS && cells[row][col + 1] == ALIVE { aliveNeighbors++ }
+
+	    // Row below
+	    if row + 1 < ROWS {
+		if col > 0 && cells[row + 1][col - 1] == ALIVE { aliveNeighbors++ }
+		if cells[row + 1][col] == ALIVE { aliveNeighbors++ }
+		if col + 1 < COLUMNS && cells[row + 1][col + 1] == ALIVE { aliveNeighbors++ }
+	    }
+
+	    if aliveNeighbors == 3 && cells[row][col] == DEAD { cells[row][col] = ALIVE }
+	    if !(2 <= aliveNeighbors && aliveNeighbors <= 3) && cells[row][col] == ALIVE { cells[row][col] = DEAD }
 	}
     }
+
     return cells
 }
 
@@ -54,9 +75,11 @@ func main() {
     drawBoard(cells)
 
     for true {
+	fmt.Println()
 	time.Sleep(time.Second)
-	// Clear terminal
-	fmt.Print("\033[H\033[2J")
+
+	// Optionally clear terminal after each iteration
+	// fmt.Print("\033[H\033[2J")
 
 	cells = updateBoard(cells)
 	drawBoard(cells)
